ac26b6c05f8fa9c8fc0e1c69fc976b26163932c1
kp1153 <kamta_tiwari@yahoo.com>
2026-04-18 16:06:26 +0530

    force deploy

--- electron.js
+++ electron.js
@@ -1,27 +1,8 @@
 const { app, BrowserWindow } = require("electron");
-const { spawn } = require("child_process");
-const path = require("path");
-const http = require("http");
+
+const VERCEL_URL = "https://nishant-ten.vercel.app";
 
 let mainWindow;
-let nextProcess;
-
-function waitForServer(url, retries, callback) {
-  http.get(url, () => callback()).on("error", () => {
-    if (retries === 0) return;
-    setTimeout(() => waitForServer(url, retries - 1, callback), 1000);
-  });
-}
-
-function startNextServer() {
-  const nextPath = path.join(__dirname, "node_modules", ".bin", "next");
-  nextProcess = spawn(nextPath, ["start", "-p", "3000"], {
-    cwd: __dirname,
-    shell: true,
-    stdio: "ignore",
-    env: { ...process.env, NODE_ENV: "production" },
-  });
-}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -32,6 +13,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      webSecurity: false,
     },
     title: "निशांत - हार्डवेयर प्रबंधन",
     autoHideMenuBar: true,
@@ -41,19 +23,14 @@ function createWindow() {
   mainWindow.once("ready-to-show", () => mainWindow.show());
   mainWindow.on("closed", () => {
     mainWindow = null;
-    if (nextProcess) nextProcess.kill();
     app.quit();
   });
 
-  startNextServer();
-  waitForServer("http://localhost:3000", 30, () => {
-    mainWindow.loadURL("http://localhost:3000");
-  });
+  mainWindow.loadURL(VERCEL_URL);
 }
 
 app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
-  if (nextProcess) nextProcess.kill();
   app.quit();
 });
